62439b3d64a5ff24266b1d894d6ab134c6896f32
Shubham Gujarathi <48732019+sbgprojects@users.noreply.github.com>
2021-09-12 17:43:26 +0530

    lazy

--- js/main.js
+++ js/main.js
@@ -2,3 +2,40 @@
 window.addEventListener("load", () => {
    // 
 });
+
+// ! ---------------------------------
+// ! Lazy Load
+// ! ---------------------------------
+const lottieImages = document.querySelectorAll('[data-src]');
+
+
+var imgOptions = {
+   threshold: 1
+}
+function preloadImage(img) {
+   const src = img.getAttribute("data-src");
+   if (!src) {
+      return;
+   }
+   var animation = bodymovin.loadAnimation({
+      container: img,
+      renderer: 'svg',
+      loop: true,
+      autoplay: true,
+      path: src
+   })
+}
+var imgObserver = new IntersectionObserver((entries, imgObserver) => {
+   entries.forEach(entry => {
+      if (!entry.isIntersecting) {
+         return;
+      } else {
+         preloadImage(entry.target);
+         imgObserver.unobserve(entry.target);
+      }
+   })
+}, imgOptions);
+
+lottieImages.forEach(image => {
+   imgObserver.observe(image);
+})
